0325e4fd09cd588c4506375f0fb3cbc83946128e
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-02-14 17:45:01 +0100

    basics

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+let bookRouter = express.Router()
+
+bookRouter.route('/books')      // localhost:3000/api/books
+    .get((req, res) => {
+        let responseJson = {hello: "This is my api"}
+        res.json(responseJson)
+    })
+
+app.use('/api', bookRouter)
+
 app.get('/', (req, res) => {
     res.send('welcome to my API!')
 })
