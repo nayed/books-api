a8919b6c4b7430c3fecd497025ddde08681a4d93
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-02-15 23:25:04 +0100

    DELETE

--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-let bookRouter = (Book) => {
+let bookRouter = Book => {
     let bookRoute = express.Router()
 
     bookRoute.route('/')              // localhost:3000/api/books
@@ -11,33 +11,30 @@ let bookRouter = (Book) => {
         })
         .get((req, res) => {
             let query = {}                  // localhost:3000/api/books?genre=Shonen or ?author=Tite Kubo
-            if (req.query.genre) {
+            if (req.query.genre) 
                 query.genre = req.query.genre
-            }
+            
             Book.find(query, (err, books) => {
-                if (err) {
+                if (err) 
                     res.status(500).send(err)
-                }
-                else {
+                else 
                     res.json(books)
-                }
             })
         })
 
     bookRoute.use('/:bookId', (req, res, next) => {     //middleware for bookId
         Book.findById(req.params.bookId, (err, book) => {
-            if (err) {
+            if (err) 
                 res.status(500).send(err)
-            }
             else if (book) {
                 req.book = book
                 next()
             }
-            else {
+            else 
                 res.status(404).send('no book found')
-            }
         })
     })
+
     bookRoute.route('/:bookId')      // http://localhost:3000/api/books/IDNUMBERSTUFF
         .get((req, res) => {
             res.json(req.book)
@@ -48,28 +45,32 @@ let bookRouter = (Book) => {
             req.book.genre = req.body.genre
             req.book.read = req.body.read
             req.book.save(err => {
-                if (err) {
+                if (err) 
                     res.status(500).send(err)
-                }
-                else {
+                else 
                     res.json(req.book)
-                }
             })
         })
         .patch((req, res) => {
-            if (req.body._id) {
+            if (req.body._id) 
                 delete req.body._id
-            }
+            
             for (let p in req.body) {
                 req.book[p] = req.body[p]
             }
             req.book.save(err => {
-                if (err) {
+                if (err) 
                     res.status(500).send(err)
-                }
-                else {
+                else 
                     res.json(req.book)
-                }
+            })
+        })
+        .delete((req, res) => {
+            req.book.remove(err => {
+                if (err) 
+                    res.status(500).send(err)
+                else 
+                    res.status(204).send('Removed')
             })
         })
     return bookRoute
