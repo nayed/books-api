b4db90a23005e0f51e6cb6a1291dedd9502e2cee
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-02-16 23:40:49 +0100

    refac

---
+++ api/controllers/bookController.js
@@ -0,0 +1,21 @@
+export let bookController = Book => {
+    let post = (req, res) => {
+        let book = new Book(req.body)
+        book.save()
+        res.status(201).send(book)
+    }
+
+    let get = (req, res) => {
+        let query = {}                          // localhost:3000/api/books?genre=Shonen or ?author=Tite Kubo
+        if (req.query.genre) 
+            query.genre = req.query.genre
+        
+        Book.find(query, (err, books) => {
+            if (err) 
+                res.status(500).send(err)
+            else 
+                res.json(books)
+        })
+    }
+    return {post, get}
+}
--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -1,28 +1,15 @@
 import express from 'express'
+import {bookController} from '../controllers/bookController'
 
 let bookRouter = Book => {
     let bookRoute = express.Router()
+    let bookCtrl = bookController(Book)
 
-    bookRoute.route('/')              // localhost:3000/api/books
-        .post((req, res) => {
-            let book = new Book(req.body)
-            book.save()
-            res.status(201).send(book)
-        })
-        .get((req, res) => {
-            let query = {}                  // localhost:3000/api/books?genre=Shonen or ?author=Tite Kubo
-            if (req.query.genre) 
-                query.genre = req.query.genre
-            
-            Book.find(query, (err, books) => {
-                if (err) 
-                    res.status(500).send(err)
-                else 
-                    res.json(books)
-            })
-        })
+    bookRoute.route('/')                            // localhost:3000/api/books
+        .post(bookCtrl.post)
+        .get(bookCtrl.get)
 
-    bookRoute.use('/:bookId', (req, res, next) => {     //middleware for bookId
+    bookRoute.use('/:bookId', (req, res, next) => {     // middleware for bookId
         Book.findById(req.params.bookId, (err, book) => {
             if (err) 
                 res.status(500).send(err)
@@ -35,7 +22,7 @@ let bookRouter = Book => {
         })
     })
 
-    bookRoute.route('/:bookId')      // http://localhost:3000/api/books/IDNUMBERSTUFF
+    bookRoute.route('/:bookId')                         // http://localhost:3000/api/books/IDNUMBERSTUFF
         .get((req, res) => {
             res.json(req.book)
         })
